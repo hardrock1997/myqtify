52a1deddef8f83c153979173254eb351b67844ce
vicfirth@1997 <yashmundra1997@gmail.com>
2023-11-07 19:57:50 +0530

    hero-qtify-takehome

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import '../components/Card.css'
+import cardImage from '../assets/Frame 1.png'
+export default function Card() {
+    return (
+        <div>
+            <img src={cardImage} alt='Card_image'/>
+        </div>
+    )
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,17 @@ import Search from './Search'
 import Hero from './Hero'
 
 export default function Navbar() {
-    return (
-    <>
+ return (
+<>
      <nav className='nav'>
         <Logo/>
         <Search placeholderText='Search a album of your choice'/>
         <Button text='Give Feedback'/>
      </nav>
-     <Hero heroText='100 Thousand Songs,ad-free Over thousands podcast episodes'/>
-    </>
-    )
-}
+    <main>
+        <section>
+            <Hero heroText='100 Thousand Songs,ad-free Over thousands podcast episodes'/>
+        </section>
+    </main>
+</>
+)}
