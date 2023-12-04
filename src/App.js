77d72fe3654cfc59bfc70a747e5049413ddd0456
vicfirth@1997 <yashmundra1997@gmail.com>
2023-12-04 15:07:23 +0530

    cardCarouselNewAlbums-qtify-takehome

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Hero from '../src/components/Hero/Hero';
 import styles from './App.module.css';
 import Section from './components/Section/Section';
 import useGetAlbumsData from '../src/hooks/useGetAlbumsData';
-import {TOPALBUMS_URL} from '../src/utils/constants'
+import {TOPALBUMS_URL} from '../src/utils/constants';
+import { NEWALBUMS_URL } from '../src/utils/constants';
 
 function App() {
 
   const [topAlbums] = useGetAlbumsData(TOPALBUMS_URL);
+  const [newAlbums] = useGetAlbumsData(NEWALBUMS_URL);
 
   return (
     <div>
@@ -15,6 +17,7 @@ function App() {
       <Hero heroText='100 Thousand Songs,ad-free Over thousands podcast episodes'/>
       <div className={styles.sectionWrapper}>
       <Section title="Top Albums" data={topAlbums} type="album"/>
+      <Section title="New Albums" data={newAlbums} type="album"/>
       </div>
     </div>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
 export const TOPALBUMS_URL = "https://qtify-backend-labs.crio.do/albums/top";
+export const NEWALBUMS_URL = "https://qtify-backend-labs.crio.do/albums/new";
